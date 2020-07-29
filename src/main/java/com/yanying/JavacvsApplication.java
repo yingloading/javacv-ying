7d43b6a6e32b46a4fd6e39600ec3582ca16f4b67
yingloading <yanying876@gmail.com>
2020-07-29 22:06:18 +0800

    初始化

---
+++ src/main/java/com/yanying/JavacvsApplication.java
@@ -0,0 +1,13 @@
+package com.yanying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacvsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavacvsApplication.class, args);
+    }
+
+}
